69bffbdf5cb478e633ed4e5042ec5526d381ad1e
dudaalvesleao <eduardaalvesleao7@gmail.com>
2024-08-15 11:57:21 -0300

    adicionando arquivo css e js

---
+++ sketch.js
@@ -0,0 +1,39 @@
+let palavra;
+
+function setup() {
+  createCanvas(400, 400);
+
+  palavra = palavraAleatoria();
+}
+
+function palavraAleatoria(){
+  let palavras = ["Bom dia!", "Boa tarde!", "Boa noite!"];
+  return random(palavras);
+}
+
+function inicializaCores() {
+
+  background("pink");
+  fill("purple");
+  textSize(74);
+  textAlign(CENTER, CENTER);
+}
+
+function palavraParcial(minimo, maximo) {
+  let quantidade = map(mouseX, minimo, maximo, 1, palavra.length);
+  let parcial = palavra.substring(0, quantidade);
+  return parcial;
+}
+
+function draw() {
+
+  inicializaCores();
+
+  let texto = palavraParcial(0, width);
+  text(texto, 200, 200);
+}
+
+
+
+
+
